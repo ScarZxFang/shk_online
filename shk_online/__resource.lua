3e76de09d944d7e6ed3a1ada8570aa4950bbd312
sσннкα <40715791+Sohhka@users.noreply.github.com>
2019-12-21 20:35:15 +0100

    Add files via upload

---
+++ shk_online/__resource.lua
@@ -0,0 +1,15 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+files {
+    'index.html',
+    'style.css',
+    'img/background.png',
+    'img/infos.png',
+    'img/devlogs.png',
+    'img/images.png',
+    'img/load.gif',
+    'img/cursor.png',
+    'Loading.ogg',
+}
+
+loadscreen 'index.html'
